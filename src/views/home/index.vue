d35559317db4bc9a5c90ba50409a4ddd4056aebb
Ace <Ace>
2026-01-03 21:59:29 +0800

    fix: 添加动画

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -59,15 +59,93 @@
         </div>
       </h2>
     </div>
+    <div class="marquee overflow-hidden px-4 sm:rounded-xl sm:bg-layer3 sm:px-3">
+      <div class="marquee-track recent-big-win inline-flex items-center gap-3 sm:gap-3.5">
+        <a
+          class="sm:w-13 flex h-28 w-14 flex-none flex-col items-center text-xs hover:opacity-80 sm:h-[106px] inactive"
+          v-for="(item, idx) in duplicatedList"
+          :key="idx"
+        >
+          <div class="relative mb-1 w-full rounded-lg pt-[133%]">
+            <img src="./img/gameImg.png" class="absolute left-0 top-0 w-full rounded-lg" />
+          </div>
+          <div class="w-[118%]">
+            <div class="flex items-center justify-center font-extrabold text-secondary">
+              <img class="size-[0.875rem]" :src="icon" /><span class="ellipsis -ml-0.5 text-xxs"
+                >BETPORTALL</span
+              >
+            </div>
+            <div
+              class="whitespace-nowrap text-nowrap text-center font-extrabold text-brand text-xxs"
+            >
+              {{ item }}
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
     <div class="overflow-hidden px-4 sm:rounded-xl sm:bg-layer3 sm:px-3">
-      <div class="marquee">
-        <div class="marquee-track">
-          <a
-            class="sm:w-13 flex h-28 w-14 flex-none flex-col items-center text-xs hover:opacity-80 sm:h-[106px] inactive"
-            v-for="(item, idx) in duplicatedList"
-            :key="idx"
-            >{{ item }}</a
-          >
+      <div class="-mx-4 bg-layer2">
+        <div class="flex w-full flex-col items-stretch gap-2 sm:mt-6 lg:!gap-3">
+          <div class="flex flex-3 gap-2 lg:!gap-3">
+            <button
+              class="button button-m center relative h-32 flex-1 overflow-hidden rounded-xl bg-layer4 p-1.5 font-extrabold sm:h-[176px] sm:p-5 col-span-2 col-start-1"
+              type="button"
+              style="background-image: linear-gradient(to left, rgb(44, 80, 64), transparent 75%)"
+            >
+              <img class="absolute right-5 top-0 h-[120%]" src="./img/casino.png" />
+              <div class="relative z-10 flex h-full flex-auto flex-col">
+                <div class="flex items-center">
+                  <div class="color_icon_img casino" style="transform: scale(1)"></div>
+                  <h2 class="ml-0.5 text-sm sm:text-[22px]">娱乐城</h2>
+                </div>
+                <div class="mb-1.5 mt-auto max-w-60 text-left font-semibold text-primary block">
+                  尽情体验BC原创游戏、真人娱乐城和老虎机游戏。
+                </div>
+              </div>
+            </button>
+            <button
+              class="button button-m center relative h-32 flex-1 overflow-hidden rounded-xl bg-layer4 p-1.5 font-extrabold sm:h-[176px] sm:p-5 col-span-2 col-start-1"
+              type="button"
+              style="background-image: linear-gradient(to left, rgb(88, 46, 89), transparent 75%)"
+            >
+              <img class="absolute right-5 top-0 h-[120%]" src="./img/sports.png" />
+              <div class="relative z-10 flex h-full flex-auto flex-col">
+                <div class="flex items-center">
+                  <div class="color_icon_img sports" style="transform: scale(1)"></div>
+                  <span class="ml-0.5 text-sm sm:text-[22px]">体育</span>
+                </div>
+                <div class="mb-1.5 mt-auto max-w-60 text-left font-semibold text-primary block">
+                  快来下注！足球、板球、NFL、电竞等 80 多种体育项目等你挑战！
+                </div>
+              </div>
+            </button>
+          </div>
+
+          <div class="flex flex-4 flex-wrap gap-2 lg:!gap-3">
+            <button
+              class="button button-m center relative h-24 flex-1 overflow-hidden rounded-xl bg-layer4 p-2 font-extrabold sm:h-[120px]"
+              type="button"
+              style="
+                background-image: linear-gradient(
+                  to left,
+                  rgba(39, 232, 187, 0.2),
+                  transparent 75%
+                );
+              "
+            >
+              <img
+                class="absolute left-[50%] top-0 h-full -translate-x-[10%]"
+                src="./img/poker.png"
+              />
+              <div class="absolute left-2 top-2 items-center flex">
+                <div class="color_icon_img bcpoker" style="transform: scale(0.8)">
+                  <img src="./img/image.png" alt="" />
+                </div>
+                <h2 class="ml-1 text-lg font-extrabold">扑克</h2>
+              </div>
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -76,6 +154,15 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import icon from './img/icon.svg?url'
+
+// const listImg = [
+//   {
+//     name: 'BETPORTALL',
+//     img: icon,
+//     icon: icon
+//   }
+// ]
 const carouselVal = ref(0)
 const carousel = (val: any) => {
   carouselVal.value = val
@@ -96,16 +183,31 @@ const duplicatedList = computed(() => [...list.value, ...list.value])
 .text-secondary {
   color: #b3bec1;
 }
-
+.ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.text-xxs {
+  font-size: 0.75rem;
+  line-height: 1rem;
+  transform: scale(0.833);
+}
 .marquee {
   overflow: hidden;
   width: 100%;
+  background-color: #292d2e;
+}
+.text-brand {
+  color: #24ee89;
 }
 .marquee-track {
+  margin: 10px 0;
+  background-color: #292d2e;
   display: inline-flex;
-  gap: 0.75rem;
+  gap: 0.875rem;
   /* 控制速度 */
-  animation: marquee 12s linear infinite;
+  animation: marquee 20s linear infinite;
   will-change: transform;
 }
 .marquee-track:hover {
@@ -113,6 +215,9 @@ const duplicatedList = computed(() => [...list.value, ...list.value])
 }
 .marquee-track a {
   flex: none;
+}
+.bg-layer4 {
+  background-color: #323738;
 }
 @keyframes marquee {
   from {
