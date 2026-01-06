50fb75b95a6f56ce234da510192fb42bb98fbeb6
mosi <kl.mosi.251225@gmail.com>
2026-01-06 16:53:01 +0800

    fix casino

--- src/views/fun/casino/index.vue
+++ src/views/fun/casino/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="casino-page">娱乐城</div>
+  <div class="casino-page">
+    <div class="banner-bg">
+        <div>
+          <img src="/src/static/img/casino/banner_bg.webp" alt="casino"/>
+          <div>
+            <h1>Stay Untamed</h1>
+            <h2>
+              <p>Sign Up & Get Up To</p>
+              <span>1,176,029.77</span>
+              <p>in Casino or Sports</p>
+            </h2>
+            <button type="button">Join Now</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <input placeholder="Search games">
+      </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
