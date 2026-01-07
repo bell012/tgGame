6ac764f6611374f9ba5fe44950cb3f962b7e677a
Ace <Ace>
2026-01-07 19:14:41 +0800

    fix 适配h5

---
+++ src/views/home/components/gameList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="mt-2 flex items-center sm:mt-6 h-8">
+      <h2 class="flex items-center text-base font-extrabold text-primary">BC 原创</h2>
+      <a
+        href="/gamelist/brand"
+        class="button ml-auto flex items-center gap-1 rounded-lg font-extrabold h-8 bg-black_alpha5 px-2 dark:bg-layer5 inactive"
+        link=""
+        >全部</a
+      >
+      <div class="ml-2 flex gap-x-1">
+        <button class="button button-second button-m size-8 bg-layer5 !p-0" type="button">
+          <div class="icon size-4">
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M20.9717 9.59292L15.2482 15.3155L20.9717 21.0389L18.5143 23.4972L10.3325 15.3164L18.5143 7.1355L20.9717 9.59292Z"
+              ></path>
+            </svg>
+          </div></button
+        ><button class="button button-second button-m size-8 bg-layer5 !p-0" type="button">
+          <div class="icon size-4 rotate-180">
+            <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M20.9717 9.59292L15.2482 15.3155L20.9717 21.0389L18.5143 23.4972L10.3325 15.3164L18.5143 7.1355L20.9717 9.59292Z"
+              ></path>
+            </svg>
+          </div>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss"></style>
