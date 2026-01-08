e0e28b2b58cc26ff1df827c5fb5094a1780cb623
jim <lucas@pgmkiun.com>
2026-01-08 17:58:35 +0700

    fix: 下载页面，pc布局

---
+++ src/views/app-download/AppDownloadCore.vue
@@ -0,0 +1,27 @@
+<template>
+  <slot
+    :download-links="downloadLinks"
+    :handle-download="handleDownload"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// 下载链接配置
+const downloadLinks = ref({
+  ios: '',
+  android: ''
+})
+
+// 下载
+const handleDownload = (platform: 'ios' | 'android') => {
+  console.log('下载:', platform)
+}
+
+defineExpose({
+  downloadLinks,
+  handleDownload
+})
+</script>
+
