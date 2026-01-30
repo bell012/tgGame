da4c97caead436dce5c21456653c19436d8be668
luosi <luo840468@gmail.com>
2026-01-30 17:25:09 +0700

    feat: 游戏详情开发10%

---
+++ src/components/game/detail/currency-bar/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-[var(--color-background-level-6)] p-[12px]">
+    <div class="flex justify-between items-center">
+      <img alt="" class="size-[16px]" :src="LineIcon" />
+
+      <div class="flex justify-end items-center gap-[10px]">
+        <img
+          alt=""
+          class="size-[16px]"
+          :src="starActived ? StarActiveIcon : StarIcon"
+          @click="toggleStar"
+        />
+        <img
+          alt=""
+          class="size-[16px]"
+          :src="loveActived ? LoveActiveIcon : LoveIcon"
+          @click="toggleLove"
+        />
+        <img alt="" class="size-[16px]" :src="TgIcon" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import LineIcon from '@/static/svg/game/detail/lines.svg?url'
+import StarIcon from '@/static/svg/game/detail/star.svg?url'
+import LoveIcon from '@/static/svg/game/detail/love.svg?url'
+import TgIcon from '@/static/svg/game/detail/tg.svg?url'
+import StarActiveIcon from '@/static/svg/game/detail/star_active.svg?url'
+import LoveActiveIcon from '@/static/svg/game/detail/love_active.svg?url'
+import { ref } from 'vue'
+
+const starActived = ref(false)
+const loveActived = ref(false)
+
+const toggleStar = () => {
+  starActived.value = !starActived.value
+}
+
+const toggleLove = () => {
+  loveActived.value = !loveActived.value
+}
+</script>
+<style lang="sass" scoped></style>
---
+++ src/views/game/detail/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <game-detail></game-detail>
+</template>
+<script setup lang="ts">
+import GameDetail from '@/components/game/detail/index.vue'
+</script>
+<style scoped lang="scss"></style>
