0e90d9a246cb1fb6da1f74263aa62e08a79b3547
luosi123456 <luosi123456@gmail.com>
2026-02-02 11:40:06 +0700

    feat: 整理目录结构

--- src/views/game/detail/index.vue
+++ src/views/game/detail/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <game-detail></game-detail>
+  <div class="w-full h-full">
+    <h5-header>Queen of Bounty</h5-header>
+    <div class="mt-[12px] rounded-md overflow-hidden">
+      <currency-info></currency-info>
+      <currency-bar></currency-bar>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-import GameDetail from '@/components/game/detail/index.vue'
+import H5Header from './h5/header.vue'
+import CurrencyInfo from './h5/currency-info/index.vue'
+import CurrencyBar from './currency-bar/index.vue'
 </script>
 <style scoped lang="scss"></style>
