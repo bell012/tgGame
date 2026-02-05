52a686721b3b6738386937b727f496ef3f8bd679
luosi123456 <luosi123456@gmail.com>
2026-02-05 20:11:01 +0700

    feat: 详情页开发

--- src/views/game/detail/recent-games/review/index.vue
+++ src/views/game/detail/recent-games/review/index.vue
@@ -50,10 +50,115 @@
         </div>
       </div>
     </div>
+    <div class="flex justify-between items-center mt-[20px]">
+      <div class="text-[12px] text-[var(--color-text-level-2)]">Comments</div>
+      <img alt="" :src="SanIcon" class="size-[18px]" />
+    </div>
+    <div
+      class="flex h-[50px] justify-between items-center bg-[var(--color-background-level-1)] rounded-[10px] mb-[10px] mt-[20px] p-[4px] px-[12px]"
+    >
+      <div class="text-[12px] text-[var(--color-text-level-2)]">Leave your Comment</div>
+      <img alt="" :src="EmoIcon" class="size-[18px]" />
+    </div>
+    <div
+      class="flex flex-col bg-[var(--color-background-level-1)] rounded-[10px] mb-[10px] mt-[10px] py-[12px] px-[12px]"
+    >
+      <div class="flex justify-between">
+        <div class="flex text-[12px] items-center gap-[8px]">
+          <img alt="" :src="PersonIcon" class="size-[26px] rounded-[26px]" />
+          <div class="text-[var(--color-text-level-2)]">Bcangcut</div>
+          <div class="text-[var(--color-text-level-3)]">245d</div>
+        </div>
+        <div class="flex items-center gap-[10px]">
+          <div>
+            <img alt="" :src="CommentIcon" class="size-[16px]" />
+          </div>
+          <div class="relative">
+            <img alt="" :src="ZanIcon" class="size-[16px]" />
+            <div
+              class="absolute text-[12px] text-[var(--color-text-level-4)] bg-[var(--color-theme-level-1)] top-[-120%] left-[50%] py-[0px] px-[10px] rounded-md"
+            >
+              8
+            </div>
+          </div>
+          <div>
+            <img alt="" :src="UnzanIcon" class="size-[16px]" />
+          </div>
+        </div>
+      </div>
+      <div class="text-[var(--color-text-level-2)] text-[12px] mt-[10px]">
+        Well, I played it and actually won. Lucky me.
+      </div>
+    </div>
+    <div
+      class="flex flex-col bg-[var(--color-background-level-1)] rounded-[10px] mb-[10px] mt-[10px] py-[12px] px-[12px]"
+    >
+      <div class="flex justify-between">
+        <div class="flex text-[12px] items-center gap-[8px]">
+          <img alt="" :src="PersonIcon" class="size-[26px] rounded-[26px]" />
+          <div class="text-[var(--color-text-level-2)]">Bcangcut</div>
+          <div class="text-[var(--color-text-level-3)]">245d</div>
+        </div>
+        <div class="flex items-center gap-[10px]">
+          <div>
+            <img alt="" :src="CommentIcon" class="size-[16px]" />
+          </div>
+          <div class="relative">
+            <img alt="" :src="ZanIcon" class="size-[16px]" />
+            <div
+              class="absolute text-[12px] text-[var(--color-text-level-4)] bg-[var(--color-theme-level-1)] top-[-120%] left-[50%] py-[0px] px-[10px] rounded-md"
+            >
+              8
+            </div>
+          </div>
+          <div>
+            <img alt="" :src="UnzanIcon" class="size-[16px]" />
+          </div>
+        </div>
+      </div>
+      <div class="text-[var(--color-text-level-2)] text-[12px] mt-[10px]">
+        Well, I played it and actually won. Lucky me.
+      </div>
+    </div>
+    <div
+      class="flex flex-col bg-[var(--color-background-level-1)] rounded-[10px] mb-[10px] mt-[10px] py-[12px] px-[12px]"
+    >
+      <div class="flex justify-between">
+        <div class="flex text-[12px] items-center gap-[8px]">
+          <img alt="" :src="PersonIcon" class="size-[26px] rounded-[26px]" />
+          <div class="text-[var(--color-text-level-2)]">Bcangcut</div>
+          <div class="text-[var(--color-text-level-3)]">245d</div>
+        </div>
+        <div class="flex items-center gap-[10px]">
+          <div>
+            <img alt="" :src="CommentIcon" class="size-[16px]" />
+          </div>
+          <div class="relative">
+            <img alt="" :src="ZanIcon" class="size-[16px]" />
+            <div
+              class="absolute text-[12px] text-[var(--color-text-level-4)] bg-[var(--color-theme-level-1)] top-[-120%] left-[50%] py-[0px] px-[10px] rounded-md"
+            >
+              8
+            </div>
+          </div>
+          <div>
+            <img alt="" :src="UnzanIcon" class="size-[16px]" />
+          </div>
+        </div>
+      </div>
+      <div class="text-[var(--color-text-level-2)] text-[12px] mt-[10px]">
+        Well, I played it and actually won. Lucky me.
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import Star from './star.vue'
 import ProgressBar from './progress.vue'
 import PersonIcon from '@/static/svg/game/detail/comment/person.svg?url'
+import SanIcon from '@/static/svg/game/detail/comment/san.svg?url'
+import EmoIcon from '@/static/svg/game/detail/comment/emo.svg?url'
+import CommentIcon from '@/static/svg/game/detail/comment/comment.svg?url'
+import ZanIcon from '@/static/svg/game/detail/comment/zan.svg?url'
+import UnzanIcon from '@/static/svg/game/detail/comment/unzan.svg?url'
 </script>
