eb47a7d6847b8b86a0205c2cd78d485a444cbea9
zhangjiakai <luo840468@gmail.com>
2026-03-05 11:37:36 +0700

    feat: 游戏详情页PC端适配开发

---
+++ src/views/game/detail/desktop/currency-info/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="currency-info-container">
+    <div class="image-content relative w-full h-[640px]">
+      <img
+        class="absolute top-0 left-0 w-full h-full object-cover rounded-t-[20px]"
+        alt=""
+        :src="gameImg"
+      />
+    </div>
+    <currency-bar></currency-bar>
+  </div>
+</template>
+<script setup lang="ts">
+import gameImg from '@/static/img/explore/game.png'
+import CurrencyBar from '../currency-bar/index.vue'
+</script>
+<style scoped lang="scss"></style>
