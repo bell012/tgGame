607852244bd803d838d40ce884eecf0e2416469e
jim <lucas@pgmkiun.com>
2026-03-16 16:35:37 +0700

    feat: 公共请求头

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+import { getDeviceTraceId } from './deviceId'
 
 const service: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
@@ -8,6 +10,14 @@ const service: AxiosInstance = axios.create({
   }
 })
 
+/**
+ * 获取当前语言代码
+ */
+function getLanguageCode(): string {
+  const language = localStorage.getItem('language') || 'en'
+  return language === 'zh-CN' ? 'zh' : 'en'
+}
+
 // 请求拦截器
 service.interceptors.request.use(
   (config: any) => {
@@ -15,6 +25,15 @@ service.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
+
+    // 公共请求头
+    config.headers.TraceId = getDeviceTraceId()
+    config.headers.uuid = uuidv4()
+    config.headers.site = 'gifphcb9'
+    config.headers.bundleId = '1.0.0'
+    config.headers.languageCode = getLanguageCode()
+    config.headers.channelId = '1'
+
     return config
   },
   (error: AxiosError) => {
