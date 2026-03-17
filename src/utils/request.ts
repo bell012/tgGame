967eb3cd2f79ec084e6d617af595f8d55cbb3bbd
jim <lucas@pgmkiun.com>
2026-03-17 13:46:47 +0700

    feat: 加密解密

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 import { getDeviceTraceId } from './deviceId'
+import { AESUtils } from './encrypt'
+import { StringExtension } from './string-extension'
 
 const service: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
@@ -25,14 +27,28 @@ service.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
-
+    const requestUuid = uuidv4()
     // 公共请求头
     config.headers.TraceId = getDeviceTraceId()
-    config.headers.uuid = uuidv4()
+    config.headers.uuid = requestUuid
     config.headers.site = 'gifphcb9'
     config.headers.bundleId = '1.0.0'
     config.headers.languageCode = getLanguageCode()
     config.headers.channelId = '1'
+
+    // 加密请求数据
+    if (config.data && config.method === 'post') {
+      try {
+        const md5Uuid = StringExtension.md5(requestUuid)
+        const encryptKey = StringExtension.tail16(md5Uuid)
+        const jsonData = JSON.stringify(config.data)
+        const encryptedData = AESUtils.encryptAES(jsonData, encryptKey)
+        config.data = encryptedData
+        config.headers['Content-Type'] = 'text/plain'
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
     return config
   },
@@ -49,6 +65,22 @@ service.interceptors.response.use(
     if (response.status !== 200) {
       console.error('Response error:', res.message || 'Error')
       return Promise.reject(new Error(res.message || 'Error'))
+    }
+
+    // 解密响应数据
+    if (typeof res === 'string' && res.length > 0) {
+      try {
+        const requestUuid = response.config.headers?.uuid as string
+        if (requestUuid) {
+          const md5Uuid = StringExtension.md5(requestUuid)
+          const decryptKey = StringExtension.tail16(md5Uuid)
+          const decryptedData = AESUtils.decryptAES(res, decryptKey)
+          return decryptedData
+        }
+      } catch (error) {
+        console.error(error)
+        return res
+      }
     }
 
     return res
