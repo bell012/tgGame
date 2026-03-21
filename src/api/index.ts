5fdaaf25352ff1f8513003ff8390ac3af7d67ab4
jim <lucas@pgmkiun.com>
2026-03-21 18:05:36 +0700

    feat: 处理打包报错问题

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,14 +4,17 @@
  * 调用示例：
  *   - Api.auth.login(data)
  *   - Api.user.queryAcctInfo(data)
+ *   - Api.home.getGameData()
  */
 
 import * as auth from './modules/auth'
 import * as user from './modules/user'
+import * as home from './modules/home'
 
 const Api = {
   auth, // 登录、注册、短信验证码
-  user // 用户信息查询
+  user, // 用户信息查询
+  home // 首页数据
 }
 
 export default Api
