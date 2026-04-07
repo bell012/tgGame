d8896012fdfd45dafb24737df20210681534ee1e
luosi123456 <luosi123456@gmail.com>
2026-04-07 14:58:02 +0700

    feat: 新增移动端意见反馈菜单路由页面

---
+++ src/views/personalCenter/feedback/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fixed inset-0 overflow-y-auto bg-bg-1">
+    <H5Header :title="t('personalCenter.leaveFeedfack')" />
+
+    <div class="px-3.5 pb-8 pt-[20px]">
+      <div class="rounded-[12px] bg-bg-2 p-4 text-sm text-text-1">
+        {{ t('personalCenter.feedbackComingSoon') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import H5Header from '@/components/common/H5Header.vue'
+
+const { t } = useI18n()
+</script>
