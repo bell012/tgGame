ca213276524b8565743be7afa1995b5353081cca
mosi <kl.mosi.251225@gmail.com>
2026-04-07 21:33:45 +0800

    fix 提现页面流程

--- src/components/withdraw/withdrawCryptoAddAddressPop.vue
+++ src/components/withdraw/withdrawCryptoAddAddressPop.vue
@@ -8,17 +8,39 @@
     @update:form-value="handleFormValueUpdate"
     @close="emit('close')"
     @confirm="emit('confirm')"
-  />
+  >
+    <template #extra-content>
+      <div class="mt-2.5 rounded-lg bg-bg-2 p-4">
+        <div class="flex items-center justify-between">
+          <p class="text-sm sm:text-base leading-normal text-text-1">
+            {{ t('withdraw.select_network') }}
+          </p>
+          <div class="flex items-center">
+            <span class="text-sm sm:text-base font-bold leading-normal text-text-1">
+              {{ network }}
+            </span>
+            <div
+              class="ml-2 flex h-5 w-5 items-center justify-center rounded-md bg-opacity-10 text-text-1"
+            >
+              <ArrowDownIcon class="h-5 w-5" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </withdrawFormPop>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
+import ArrowDownIcon from '@/static/svg/arrow_down.svg?component'
 import withdrawFormPop from './withdrawFormPop.vue'
 
 interface Props {
   modelValue: boolean
   currencyCode: string
+  network: string
   icon: string
   inputValue: string
 }
