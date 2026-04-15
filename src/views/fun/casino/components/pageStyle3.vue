906fd78c65ebb767daa53f97ab9d0d7dc0effae0
mosi <kl.mosi.251225@gmail.com>
2026-04-15 16:31:24 +0800

    fix #124 【H5】【娱乐城】后台配置的数字越小，展示应靠前

--- src/views/fun/casino/components/pageStyle3.vue
+++ src/views/fun/casino/components/pageStyle3.vue
@@ -141,7 +141,7 @@ const resolvedQueryOptions = computed<GameQueryOptions>(() => {
     ...(props.queryOptions ?? props.modules ?? {})
   }
   const sortOptionMap: Record<string, Partial<GameQueryOptions>> = {
-    default: { sortByOrderId: true },
+    default: { sortByOrderId: true, sortDirection: 'desc' },
     'a-z': { sortByOrderId: true, sortByItemName: true, sortDirection: 'asc' },
     'z-a': { sortByOrderId: true, sortByItemName: true, sortDirection: 'desc' }
   }
