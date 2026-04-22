87b4cf14de2fc4f5aa7d5c8f9a2c3d10cd2f8ffc
mosi <kl.mosi.251225@gmail.com>
2026-04-22 16:23:35 +0800

    feat #697 【H5】【娱乐城】选中的效果和效果图不符

--- src/views/fun/casino/components/pageStyle1.vue
+++ src/views/fun/casino/components/pageStyle1.vue
@@ -119,11 +119,11 @@
         <span class="ml-auto"></span>
       </div>
       <div
-        class="mt-2 flex w-full rounded bg-opacity-6 text-text-2 sm:!-mt-9 sm:ml-auto sm:max-w-[347px]"
+        class="mt-2 flex w-full rounded-lg bg-bg-8 text-text-2 sm:!-mt-9 sm:ml-auto sm:max-w-[347px]"
       >
         <button
           :class="{
-            'bg-opacity-10 text-text-4 dark:text-text-1 ': latestBetIndex === 0
+            'bg-bg-7 text-text-1': latestBetIndex === 0
           }"
           class="flex h-10 flex-1 shrink-0 items-center justify-center rounded-lg text-sm font-bold"
           @click.stop="latestBetIndex = 0"
@@ -132,7 +132,7 @@
         </button>
         <button
           :class="{
-            'bg-opacity-10 text-text-4 dark:text-text-1 ': latestBetIndex === 1
+            'bg-bg-7 text-text-1': latestBetIndex === 1
           }"
           class="flex h-10 flex-1 shrink-0 items-center justify-center rounded-lg text-sm font-bold"
           @click.stop="latestBetIndex = 1"
