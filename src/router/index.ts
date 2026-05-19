842fe874f9f739a2da96d622aadd0ca0b314ba20
leo <leo@leodeMacBook-Pro.local>
2026-05-19 18:49:58 +0700

    fix: 邀请url兼容处理

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ import {
   getStorageLanguageCode,
   withLocalePrefix
 } from '@/utils/locale'
-import { normalizeInvitationCode, saveInvitationCode } from '@/utils/invitationAttribution'
+import { resolveInvitationCodeFromQuery, saveInvitationCode } from '@/utils/invitationAttribution'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const baseRoutes: RouteRecordRaw[] = [
@@ -782,7 +782,7 @@ router.beforeEach((to, _from, next) => {
   const routeLocaleParam = to.params.locale as string | undefined
   const routeLocale = getLocaleFromRouteParam(routeLocaleParam)
   const persistedLocale = getPersistedLocale()
-  const invitationCode = normalizeInvitationCode(to.query.id)
+  const invitationCode = resolveInvitationCodeFromQuery(to.query as Record<string, unknown>)
 
   if (routeLocaleParam && !routeLocale) {
     next(withLocalePrefix('/', persistedLocale ?? DEFAULT_LOCALE))
