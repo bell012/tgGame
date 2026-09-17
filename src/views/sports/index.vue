191c451bfa0502923f7eac5635e24961051ac777
jim <lucas@pgmkiun.com>
2026-09-17 17:38:18 +0700

    feat:赛事筛选和搜索

--- src/views/sports/index.vue
+++ src/views/sports/index.vue
@@ -3,11 +3,14 @@
     体育
     <!-- 联赛tabs -->
     <Liansai_tabs />
+    <!-- 赛事筛选和搜索 -->
+    <Filter_search class="mt-[16px]" />
   </div>
 </template>
 
 <script setup lang="ts">
 import Liansai_tabs from './components/liansai_tabs.vue'
+import Filter_search from './components/filter_search.vue'
 </script>
 
 <style scoped lang="scss"></style>
