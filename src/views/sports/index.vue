5ac442250ec77d9d96b54b0abf3516f90af603ef
jim <lucas@pgmkiun.com>
2026-09-17 16:54:57 +0700

    体育联赛tabs

--- src/views/sports/index.vue
+++ src/views/sports/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="sports-page">体育</div>
+  <div class="sports-page mt-[60px]">
+    体育
+    <!-- 联赛tabs -->
+    <Liansai_tabs />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Liansai_tabs from './components/liansai_tabs.vue'
+</script>
 
 <style scoped lang="scss"></style>
