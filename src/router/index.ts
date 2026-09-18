2c8cf7972dc5fb2ba50dea685d8f0a8a4dea33d6
luosi12345 <luo840468@gmail.com>
2026-09-18 15:46:19 +0700

    chore: 删除体育组件 Demo 页面与组件文档

--- src/router/index.ts
+++ src/router/index.ts
@@ -149,18 +149,6 @@ const baseRoutes: RouteRecordRaw[] = [
       title: '体育'
     }
   },
-  ...(import.meta.env.DEV
-    ? [
-        {
-          path: 'sports/demo',
-          name: 'sportsDemo',
-          component: () => import('@/views/sports/components/demo.vue'),
-          meta: {
-            title: '体育组件 Demo'
-          }
-        }
-      ]
-    : []),
   {
     path: 'menu',
     name: 'menu',
