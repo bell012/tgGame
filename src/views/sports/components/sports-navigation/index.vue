9246bb6984242e0d3d7fc88263624f3670ee1df1
mosi <kl.mosi.251225@gmail.com>
2026-09-19 18:36:27 +0800

    fix: 同步体育球种导航选中状态

--- src/views/sports/components/sports-navigation/index.vue
+++ src/views/sports/components/sports-navigation/index.vue
@@ -4,8 +4,18 @@
     :class="isMobile ? 'min-h-full bg-bg-1 pl-[14px]' : 'px-[24px] pt-4'"
     :style="pageStyle"
   >
-    <SportsNavigationH5 v-if="isMobile" :counts="sportTodayCounts" @change="handleSportChange" />
-    <SportsNavigationPc v-else :counts="sportTodayCounts" @change="handleSportChange" />
+    <SportsNavigationH5
+      v-if="isMobile"
+      :selected-sport-id="selectedSportId"
+      :counts="sportTodayCounts"
+      @change="handleSportChange"
+    />
+    <SportsNavigationPc
+      v-else
+      :selected-sport-id="selectedSportId"
+      :counts="sportTodayCounts"
+      @change="handleSportChange"
+    />
   </div>
 </template>
 
@@ -28,7 +38,7 @@ const emit = defineEmits<{
 const isMobile = useIsMobile()
 const layoutStore = useLayoutStore()
 const sportsStore = useSportsStore()
-const { sportCounts } = storeToRefs(sportsStore)
+const { sportCounts, selectedSportId } = storeToRefs(sportsStore)
 
 const sportTodayCounts = computed(() => buildSportTodayCountMap(sportCounts.value))
 
