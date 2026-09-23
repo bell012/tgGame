dec55f13d33b37d1b236b79a521060c519789778
Ace <ace65339@gmail.com>
2026-09-23 15:29:26 +0700

    体育详情h5适配

--- src/views/sports/event-details/index.vue
+++ src/views/sports/event-details/index.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="min-h-[200px] w-full min-w-0 bg-bg-1 px-5 py-6 font-inter text-text-1">
-    <EventDetailsTabs v-model="activeMatchId" @change="onTabChange" />
-    <MatchDetails class="mt-4" />
-    <SportsScoreDetails class="mt-4" />
+  <div
+    class="min-h-[200px] w-full min-w-0 bg-bg-1 px-[14px] pb-[13px] font-inter text-text-1 lg:px-5 lg:py-6"
+    :style="pageStyle"
+  >
+    <template v-if="!isMobile">
+      <EventDetailsTabs v-model="activeMatchId" @change="onTabChange" />
+      <MatchDetails class="mt-4" />
+    </template>
+    <SportsScoreDetails :class="isMobile ? '' : 'mt-4'" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useIsMobile } from '@/composables/useMediaQuery'
+import { useLayoutStore } from '@/stores/layout'
+import { computed, ref } from 'vue'
 import EventDetailsTabs from './components/event-detailsd-tabs/index.vue'
 import MatchDetails from './components/match-details/index.vue'
 import SportsScoreDetails from './components/sports-score-details/index.vue'
+
+const isMobile = useIsMobile()
+const layoutStore = useLayoutStore()
+
+const pageStyle = computed(() => {
+  if (!isMobile.value) {
+    return undefined
+  }
+
+  return {
+    paddingTop: `${layoutStore.TOPNAV_HEIGHT + 13}px`
+  }
+})
 
 const activeMatchId = ref('west-ham-everton')
 
